b743624ece0139274c4436856e8240ceb4c1253d
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2021-11-16 10:24:37 -0800

    clean up df structure, filter for site and date, make "date" a date object

--- check.R
+++ check.R
@@ -2,17 +2,17 @@ library(tidyverse)
 library(lubridate)
 library(here)
 
-ph_clean_updated <- read_csv(here("data", "ph_clean_1121.csv"))
+ph_clean <- read_csv(here("data", "ph_clean_1121.csv"))
+
+ph_clean <- ph_clean %>%
+  mutate(date=mdy(date))
 
 #ph_clean_updated$date =  as.Date(ph_clean_updated$date, format = "%m-%d-%Y")
 
-site_select <- ph_clean_updated %>% 
-    filter(site == "Alegria")
-
-dateFiltered <- ph_clean_updated %>%
-  filter(date %in% seq("2021-06-18",
-                       "2021-10-08",
-                       by = "day"))
+dateFiltered <- ph_clean %>%
+  filter(site == "Lompoc Landing") %>%
+  filter(date >= mdy("06/18/2021"),
+         date <= mdy("09/08/2021"))
 
 #plot
 ph_ts_plot <- ggplot(dateFiltered, aes(x=date_time, y=tide)) +
